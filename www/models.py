70ad4883f5a416f234a2be30f2c413eedd1e66f1
jun-x <abc987plm@qq.com>
2019-02-22 10:47:07 +0800

    add models

---
+++ www/models.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+'''
+Models for user, blog, commit.
+'''
+
+__author__ = 'jun-x'
+
+import time, uuid
+
+from orm import Model, StringField, BooleanField, FloatField, TextField
+
