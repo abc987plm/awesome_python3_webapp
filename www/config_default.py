a41a00af396ce96d35b6e2db8f1057d9c8ecf346
jun-x <abc987plm@qq.com>
2019-02-22 16:21:59 +0800

    add config

---
+++ www/config_default.py
@@ -0,0 +1,21 @@
+#!python3
+
+'''
+Default configurations.
+'''
+
+__author__ = 'jun-x'
+
+configs = {
+    'debug': True,
+    'db':{
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'www',
+        'password': 'www',
+        'db': 'awesome'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
---
+++ www/config_override.py
@@ -0,0 +1,13 @@
+#!python3
+
+'''
+Override configurations.
+'''
+
+__author__ = 'jun-x'
+
+configs = {
+    'db': {
+        'host': '127.0.0.1'
+    }
+}
