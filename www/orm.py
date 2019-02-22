be7b412f4eed5e8ccea9d9d0d714c1a2ba547908
jun-x <abc987plm@qq.com>
2019-02-23 00:19:34 +0800

    fix orm->primaryKey

--- www/orm.py
+++ www/orm.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python3
+#!python3
+# -*- coding: utf-8 -*-
 
 
 __author__ = 'jun-x'
@@ -85,7 +86,7 @@ class BooleanField(Field):
 
 class IntegerField(Field):
 
-    def __init__(self, name=None, primary_key=False, default=0.0):
+    def __init__(self, name=None, primary_key=False, default=0):
         super().__init__(name, 'bigint', primary_key, default)
 
 class FloatField(Field):
@@ -100,40 +101,41 @@ class TextField(Field):
 
 class ModelMetaclass(type):
 
+
     def __new__(cls, name, bases, attrs):
+
         if name=='Model':
             return type.__new__(cls, name, bases, attrs)
         tableName = attrs.get('__table__', None) or name
-        logging.info('found model: %s (table:%s)' % (name, tableName))
+        logging.info('found model: %s (table: %s)' % (name, tableName))
         mappings = dict()
         fields = []
         primaryKey = None
         for k, v in attrs.items():
             if isinstance(v, Field):
-                logging.info(' found mapping: %s ==> %s' % (k, v))
+                logging.info('  found mapping: %s ==> %s' % (k, v))
                 mappings[k] = v
                 if v.primary_key:
-                    # find primary key
+                    # 找到主键:
                     if primaryKey:
                         raise BaseException('Duplicate primary key for field: %s' % k)
                     primaryKey = k
-
                 else:
                     fields.append(k)
-            if not primaryKey:
-                raise BaseException('Primary key not found.')
-            for k in mappings.keys():
-                attrs.pop(k)
-            escaped_fields = list(map(lambda f: '`%s`' % f, fields))
-            attrs['__mappings__'] = mappings # 保存属性和列的映射关系
-            attrs['__table__'] = tableName
-            attrs['__primary_key__'] = primaryKey
-            attrs['__fields__'] = fields
-            attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
-            attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields)+1))
-            attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
-            attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
-            return type.__new__(cls, name, bases, attrs)
+        if not primaryKey:
+            raise BaseException('Primary key not found.')
+        for k in mappings.keys():
+            attrs.pop(k)
+        escaped_fields = list(map(lambda f: '`%s`' % f, fields))
+        attrs['__mappings__'] = mappings # 保存属性和列的映射关系
+        attrs['__table__'] = tableName
+        attrs['__primary_key__'] = primaryKey # 主键属性名
+        attrs['__fields__'] = fields # 除主键外的属性名
+        attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
+        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
+        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
+        attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
+        return type.__new__(cls, name, bases, attrs)
 
 class Model(dict, metaclass=ModelMetaclass):
 
@@ -192,7 +194,7 @@ class Model(dict, metaclass=ModelMetaclass):
     @classmethod
     async  def findNumber(cls, selectField, where=None, args=None):
         ' find number by select and where.'
-        sql = ['select %s _num_from `%s`' % (selectField, cls.__select__)]
+        sql = ['select %s _num_from `%s`' % (selectField, cls.__table__)]
         if where:
             sql.append('where')
             sql.append(where)
